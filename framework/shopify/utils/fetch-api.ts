9df706b9bdaec0f5a7677acc1aec51355e6ccabb
shutsuchi <wwww.www.wshu@gmail.com>
2021-10-15 09:35:56 +0900

    create app and framework base config

---
+++ framework/shopify/utils/fetch-api.ts
@@ -0,0 +1,25 @@
+import { ApiFetcherOptions, ApiFetcherResult } from '@common/types/api';
+
+const fetchApi = async <T>(
+  { url, query }: ApiFetcherOptions
+): Promise<ApiFetcherResult<T>> => {
+  const res = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': "application/json",
+    },
+    body: JSON.stringify({
+      query
+    })
+  });
+
+  const { data, errors } = await res.json();
+
+  if (errors) {
+    throw new Error(errors[0].message ?? errors.message);
+  };
+
+  return { data };
+};
+
+export default fetchApi;
---
+++ framework/shopify/utils/normalize.ts
@@ -0,0 +1,38 @@
+import {
+  ImageEdge,
+  Product as ShopifyProduct
+} from '../schema';
+
+import { Product } from '@common/types/product';
+
+const normalizeProductImages = ({ edges }: { edges: Array<ImageEdge> }) => {
+  return edges.map(({ node: { originalSrc: url, ...rest} }) => ({
+    url: `/images/${url}`,
+    ...rest,
+  }));
+};
+
+export const normalizeProduct = (productNode: ShopifyProduct): Product => {
+  const {
+    id,
+    title: name,
+    handle,
+    vendor,
+    description,
+    images: imageConnection,
+    ...rest
+  } = productNode;
+
+  const product = {
+    id,
+    name,
+    vendor,
+    description,
+    path: `/${handle}`,
+    slug: handle.replace(/^\/+|\/+$/g, ""),
+    images: normalizeProductImages(imageConnection),
+    ...rest
+  };
+
+  return product;
+};
